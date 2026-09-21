a03ca7645a3596692953a8c7f0ae706ee634c705
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-09-21 11:46:17 +0700

    Add analytics vercel web

--- taskflow-frontend/src/app/page.tsx
+++ taskflow-frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { authService } from "@/services/auth.service";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   const router = useRouter();
